8109d205b69a8a8a2299264f3960eac41121945a
Daniel <Danielsalazr@hotmail.com>
2024-08-10 14:14:32 -0500

    avances del recetario

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ type Props = {}
 export default function Footer({}: Props) {
   return (
     <footer className="pt-0 md:py-20 border-t">
-        <div className="mx-auto max-w-container max-w-7xl px-4 py-6 sm:px-6 xl:px-8">
+        <div className="mx-auto  max-w-7xl px-4 py-6 sm:px-6 xl:px-8">
           <strong className="text-xl">FoodColombia</strong>
         </div>
     </footer>
--- containers/YaEstamosAqui.tsx
+++ containers/YaEstamosAqui.tsx
@@ -8,9 +8,9 @@ type Props = {}
 
 export default function YaEstamosAqui({}: Props) {
   return (
-    <section className="flex max-w-container py-0 md:py-8 ">
-        <div className='flex flex-wrap justify-center mx-auto max-w-7xl py-4 gap-x-4'>
-            <div className="flex h-96 justify-center  p-4" >
+    <section className="max-w-container py-0 md:py-8 ">
+        <div className='flex flex-row  flex-container justify-center mx-auto max-w-7xl py-4 gap-x-4'>
+            <div className="flex-1 justify-center  p-4" >
                 <Image
                     src={comida_bienvenida}
                     alt=''
@@ -21,7 +21,7 @@ export default function YaEstamosAqui({}: Props) {
                 />
             </div>
 
-            <div className="w-96 flex flex-col h-96  justify-center  p-4">
+            <div className="flex-1 p-4 basis-full md:basis-8/12">
                 {/* <h2>Ya estamos aqui</h2> */}
                 <p className="text-4xl font-normal leading-6">
                     
